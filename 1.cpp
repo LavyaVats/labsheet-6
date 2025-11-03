338e08b6d516441ddfd5ee3c1089868a7bfd6942
lavya vats <cu25870001@coeruniversity.ac.in>
2025-11-03 09:24:30 +0530

    1.cpp

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    cout << "\nThe elements of the array are:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
